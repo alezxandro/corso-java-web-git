89fed358028592238ed9db6f37d310a491b310a3
alezxandro <gheojan.alessandro@outlook.com>
2023-11-07 16:40:31 +0100

    Inventario

---
+++ src/giocattolaio/Inventario.java
@@ -0,0 +1,31 @@
+package src.giocattolaio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventario {
+    private List<Giocattolo> giocattoli;
+
+    public Inventario() {
+        this.giocattoli = new ArrayList<>();
+    }
+
+    public Giocattolo getById(int id) {
+        for (Giocattolo giocattolo : giocattoli) {
+            if (giocattolo.getId() == id) {
+                return giocattolo;
+            }
+        }
+
+        return null;
+    }
+
+    public void addGiocattolo(Giocattolo giocattolo) {
+        giocattoli.add(giocattolo);
+    } 
+
+    public boolean removeGiocattolo (int id) {
+        return giocattoli.removeIf(g -> g.getId() == id);
+    }
+
+}
